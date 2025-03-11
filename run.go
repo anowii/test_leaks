d68fbef1dc4112d4711d1bc69bc114d5c6be5a27
anowii <91365639+anowii@users.noreply.github.com>
2025-03-11 16:12:30 +0100

    Update run.go

--- run.go
+++ run.go
@@ -5,7 +5,7 @@ import "fmt"
 
 func PrintHello() {
 	aws_token := "AKILEMEL33243OLIA"  // fingerprint of that secret is added to .gitleaksignore
-  aws_token := "mykeye9992781+ffalakw"
+  	aws_token := "mykeye9992781+ffalakw"
 	fmt.Println(aws_token)
 	fmt.Println(aws_token2)
 }
